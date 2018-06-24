57698966917fe980868b312c58e1625730e33d33
Bruce Schubert <bruce@emxsys.com>
2018-06-24 15:22:44 -0700

    Cloned from https://github.com/emxsys/worldwind-react-app

---
+++ docs/README.md
@@ -0,0 +1,69 @@
+![Emxsys](images/world.png)
+
+# [Earth Viewer - Explorer](https://earthviewer.github.io/explorer/) 
+
+[![Build Status](https://travis-ci.org/earthviewer/explorer.svg?branch=master)](https://travis-ci.org/earthviewer/explorer)
+
+The __Earth Viewer - Explorer__ is a 3D globe geo-browser web application built with the 
+[Web WorldWind](https://github.com/NASAWorldWind/WebWorldWind) 3D globe library and  
+[React](https://reactjs.org) and [Bootstrap](https://getbootstrap.com). 
+Web WorldWind is an open-source virtual globe SDK developed jointly by 
+[NASA and ESA](https://worldwind.arc.nasa.gov/web/). 
+
+This project provides a feature-rich, responsive, customizable web app ready to be deployed to your 
+site, including: 
+
+- Initializing WorldWind with 3D globes and/or 2D maps
+- Configuring and managing layers and settings
+- Place name searches and geocoding
+- Creating placemarks
+- Going to locations
+- Multi-globe support
+- Plus, automatic deployment to GitHub Pages
+
+This project was initially cloned from the [worldwind-react-app](https://github.com/emxsys/worldwind-react-app) project.
+
+Below you will find some information on how to perform common tasks.<br>
+
+## Setup
+
+After cloning this project, edit the following files to customize the branding of your app:
+
+File | Changes
+-----|--------
+__package.json__ | Edit the __name__, __version__ and __homepage__ properties.
+__src/App.js.js__ | Edit the `<NavBar />` element's title, href, and logo. 
+__public/index.html__ | Change the `<title/>` and `<meta name='description' />` elements.
+
+Then run `npm install`.
+
+
+## Scripts
+
+### `npm install`
+ 
+Installs the dependencies defined in `package.json`.
+
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br>
+Your app is ready to be deployed!
+
+
+### `npm run deploy`
+
+Deploys the app to your `gh-pages` branch.
+
---
+++ src/App.js
@@ -0,0 +1,115 @@
+/* 
+ * Copyright (c) 2018 Bruce Schubert.
+ * The MIT License
+ * http://www.opensource.org/licenses/mit-license
+ */
+import React, { Component } from 'react'
+import Globe from 'worldwind-react-globe'
+import { 
+  CardColumns, 
+  Container } from 'reactstrap'
+import { 
+  LayersCard, 
+  MarkersCard, 
+  NavBar, 
+  NavBarItem, 
+  SearchBox, 
+  SettingsCard, 
+  Tools } from 'worldwind-react-globe-bs4'
+
+import './App.css'
+
+export default class App extends Component {
+    
+  constructor(props) {
+    super(props)
+    this.state = {
+      lat: 34.2,
+      lon: -119.2,
+      alt: 10e6,
+      globe: null
+    }
+    // Set the location of the WorldWind "images" folder to the root of the public folder
+    Globe.setBaseUrl("./");    
+    
+    this.globeRef = React.createRef()
+    this.layersRef = React.createRef()
+    this.markersRef = React.createRef()
+    this.settingsRef = React.createRef()
+  }
+  
+  componentDidMount() {
+    // Get the component with the WorldWindow after mounting
+    this.setState({globe: this.globeRef.current})
+  }
+  
+  render() {
+    
+    const globe = this.globeRef.current
+    
+    const layers = [
+      {layer: 'blue-marble', options: {category: 'base', enabled: false}},
+      {layer: 'blue-marble-landsat', options: {category: 'base', enabled: false}},
+      {layer: 'bing-aerial', options: {category: 'base', enabled: false}},
+      {layer: 'bing-aerial-labels', options: {category: 'base', enabled: false}},
+      {layer: 'eox-sentinal2', options: {category: 'base', enabled: false}},
+      {layer: 'eox-sentinal2-labels', options: {category: 'base', enabled: true}},
+      {layer: 'eox-openstreetmap', options: {category: 'overlay', enabled: false, opacity: 0.8}},
+      {layer: 'bing-roads', options: {category: 'overlay', enabled: false, opacity: 0.8}},
+      {layer: 'renderables', options: {category: 'data', enabled: true, displayName: 'Markers'}},
+      {layer: 'compass', options: {category: 'setting', enabled: false}},
+      {layer: 'coordinates', options: {category: 'setting', enabled: true}},
+      {layer: 'view-controls', options: {category: 'setting', enabled: true}},
+      {layer: 'stars', options: {category: 'setting', enabled: false}},
+      {layer: 'atmosphere-day-night', options: {category: 'setting', enabled: false}}
+    ]
+    
+    const navbarItems = [
+      <NavBarItem key='lyr' title='Layers' icon='list' collapse={this.layersRef.current}/>,
+      <NavBarItem key='mkr' title='Markers' icon='map-marker' collapse={this.markersRef.current}/>,
+      <NavBarItem key='set' title='Settings' icon='cog' collapse={this.settingsRef.current}/>
+    ]
+   
+    const navbarSearch = <SearchBox globe={globe}/>
+    
+    return (
+      <div>
+        <NavBar 
+            logo='images/world.png'
+            title='Earth Viewer'
+            href='https://github.com/earthviewer/'
+            items={navbarItems}
+            search={navbarSearch} />
+        <Container fluid className='p-0'>
+          <div className='globe'>
+              <Globe 
+                ref={this.globeRef} 
+                layers={layers}/>
+          </div>
+          <div className='overlayTools noninteractive'>
+              <Tools 
+                globe={globe} 
+                markers={this.markersRef.current}
+                markersLayerName='Markers'/>
+          </div>
+          <div className='overlayCards noninteractive'>
+            <CardColumns>
+              <LayersCard
+                ref={this.layersRef}
+                categories={['overlay', 'base']} 
+                globe={globe} />
+              <MarkersCard
+                ref={this.markersRef}
+                globe={globe}
+                markersLayerName='Markers' />
+              <SettingsCard
+                ref={this.settingsRef}
+                categories={['setting']} 
+                globe={globe} />
+            </CardColumns>
+          </div>
+        </Container>
+      </div>
+    )
+  }
+}
